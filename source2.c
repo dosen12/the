57d3cc753bfb2a823e71f01a2d630b6f33b2e493
dosen12 <37952506+dosen12@users.noreply.github.com>
2018-05-18 11:35:29 +0900

    4번 째

--- source2.c
+++ source2.c
@@ -5,6 +5,12 @@ int main()
   printf("first version");
   printf("second version");
   printf("thrid version");
-
+  int a=0;
+  int i=0;
+  for(i=0; i<10; i++)
+  {
+    a+= i;
+  }
+  printf("%d" , a);
   return 0;
 }
